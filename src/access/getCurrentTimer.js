c5256f90ff72a9530d4de0207c2ece679d37ac08
Bayram Emiraliev <emiraliev.kad@gmail.com>
2024-01-13 18:49:09 +0300

    chenge the timer

--- src/access/getCurrentTimer.js
+++ src/access/getCurrentTimer.js
@@ -1,7 +1,7 @@
 export function getCurrentTimer(number) {
-  const hours = Math.floor(number / 60 / 60) % 24;
-  const minutes = Math.floor((number % 1000) / 60);
   const seconds = number % 60;
+  const hours = Math.floor(number / 60 / 60);
+  const minutes = Math.floor(number / 60) - hours * 60;
 
   const hoursCurrect = hours < 10 ? '0' + hours : hours;
   const minutesCurrect = minutes < 10 ? '0' + minutes : minutes;
